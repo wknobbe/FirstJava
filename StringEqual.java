39f03bf01e25f047ce627246072557c8cd841ff2
wknobbe <wknobbe4@gmail.com>
2020-05-09 10:10:57 -0500

    String Equal Test

---
+++ StringEqual.java
@@ -0,0 +1,6 @@
+public class StringEqual {
+    public Boolean stringEqual(String str1, String str2){
+        Boolean result = str1.equals(str2);
+        return result;
+    }
+}
---
+++ StringEqualTest.java
@@ -0,0 +1,7 @@
+public class StringEqualTest {
+    public static void main(String[] args) {
+        StringEqual test = new StringEqual();
+        Boolean answer = test.stringEqual("Hello", "World");
+        System.out.println(answer);
+    }
+}
