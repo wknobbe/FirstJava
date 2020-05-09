845d7f08f70faec16b8198eeb6e8bea3378a0572
wknobbe <wknobbe4@gmail.com>
2020-05-09 08:28:55 -0500

    First Java Project

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Bill");
+        System.out.println("I am 30 years old.");
+        System.out.println("My hometown is Palos Hills.");
+    }
+}
